26c908446fd91d3d8e7bd72c9c078f8245305e4e
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-05-05 16:48:57 +0545

    add status code

--- server/src/middlewares/Errorhandler.js
+++ server/src/middlewares/Errorhandler.js
@@ -1,7 +1,9 @@
-const errorHandler = (err,req,res,next)=>{
-  res.status(err.status || 500).json({
+const errorHandler = (err, req, res, next) => {
+  console.log('ERROR:', err.message, err.status, err.statusCode)
+  
+  res.status(err.status || err.statusCode || 500).json({
     message: err.message || 'Server Error',
-  });
+  })
 }
 
 export default errorHandler;
