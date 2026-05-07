7c63dc5bcac6cd3a871bfe71f6218a91233031fd
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-05-07 15:45:29 +0545

    changed layout format

---
+++ client/app/(main)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+  <div className="flex flex-col max-w-7xl mx-auto px-6 py-10">
+    <h1 className="text-teal-700 text-4xl font-bold">Listings</h1>
+    <p className="mt-2">Coming Soon...</p>
+  </div>
+  )
+}
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { FiSearch } from 'react-icons/fi';
+import Link from 'next/Link';
+import {useUser, UserButton} from '@clerk/nextjs';
+
+export default function Navbar(){
+
+
+
+  return(
+    <div>
+      coming soon -nav
+    </div>
+  )
+}
