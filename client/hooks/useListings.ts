8587c7fef7d11f49cf9625b71dc0484c3dc0cfa7
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-05-11 14:31:40 +0545

    remove limit

--- client/hooks/useListings.ts
+++ client/hooks/useListings.ts
@@ -11,7 +11,6 @@ export function useListings(filters: ListingFilters = {}) {
       filters.minPrice,
       filters.maxPrice,
       filters.page,
-      filters.limit,
     ],
     queryFn: () => getListings(filters),
     staleTime: 1000 * 60,
