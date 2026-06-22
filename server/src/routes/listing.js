58749805c4e21e21385cf17384261d0cd69da406
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-06-22 14:17:00 +0545

    minor fix

--- server/src/routes/listing.js
+++ server/src/routes/listing.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import{
+import {
   createListing,
   getAllListings,
   getOneListing,
@@ -9,16 +9,16 @@ import{
   getMyListings
 } from '../controllers/listingController.js';
 
-import {protect} from '../middlewares/auth.js';
+import { protect } from '../middlewares/auth.js';
 import requireRole from '../middlewares/requireRole.js';
 
 const router = express.Router();
 
-router.post('/',protect,requireRole('landlord'),createListing);
-router.get('/',getAllListings);
-router.get('/:id',getOneListing);
-router.patch('/:id',protect, requireRole('landlord'), updateListing);
-router.delete('/:id',protect,requireRole('landlord'),deleteListing);
-router.get('/mine', protect, requireRole('landlord'), getMyListings) 
+router.post('/', protect, requireRole('landlord'), createListing);
+router.get('/', getAllListings);
+router.get('/mine', protect, requireRole('landlord'), getMyListings);
+router.get('/:id', getOneListing);
+router.patch('/:id', protect, requireRole('landlord'), updateListing);
+router.delete('/:id', protect, requireRole('landlord'), deleteListing);
 
 export default router;
